538a742e3d5f0b6c690f2f98457bedc329b6b132
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-12-19 21:54:51 -0300

    update

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
 
   Results = [];
+  formGroup!: FormGroup;
 
-  constructor(){}
+  constructor(private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    })
+  }
+
+  onSubmit(): void{
+    this.formGroup.value
+
+  }
+
+  onClean(): void{
+    this.formGroup.reset
 
   }
 }
